d7739a2c3888e57db41bb8879d0b11e64293bb49
noodlebox <ash@noodlebox.moe>
2016-10-19 23:46:29 -0500

    Add DiscordLineNumbers userscript and userstyle

---
+++ DiscordLineNumbers.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         DiscordLineNumbers
+// @namespace    https://files.noodlebox.moe/
+// @downloadURL  https://files.noodlebox.moe/userscripts/DiscordLineNumbers.user.js
+// @version      1.1.0
+// @description  Add line numbers to code blocks
+// @author       noodlebox
+// @require      https://code.jquery.com/jquery-3.1.1.min.js
+// @match        *://discordapp.com/channels/*
+// @match        *://discordapp.com/invite/*
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+(function ($) {
+    "use strict";
+
+    // Helper function for finding all elements matching selector affected by a mutation
+    function mutationFind(mutation, selector) {
+        var target = $(mutation.target), addedNodes = $(mutation.addedNodes);
+        var mutated = target.add(addedNodes).filter(selector);
+        var descendants = addedNodes.find(selector);
+        var ancestors = target.parents(selector);
+        return mutated.add(descendants).add(ancestors);
+    }
+
+    // Watch for new code blocks
+    new MutationObserver(function (mutations, observer) {
+        mutations.forEach(function (mutation) {
+            mutationFind(mutation, ".hljs").not(":has(ol)")
+                .each(function () {
+                    this.innerHTML = this.innerHTML.split("\n").map(line => "<li>"+line+"</li>").join("");
+                })
+                .wrapInner($("<ol>").addClass("kawaii-linenumbers"));
+        });
+    }).observe(document, { childList:true, subtree:true });
+})(jQuery.noConflict(true));
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # noodlebox/userscripts
-Assorted userscripts, too small to warrant their own repos. These scripts are also hosted at and set to update from [here](https://files.noodlebox.moe/userscripts/). You will most likely need a userscript manager, such as [Tampermonkey](https://tampermonkey.net/), which should be available for whichever browser you use.
+Assorted userscripts, too small to warrant their own repos. These scripts are also hosted at and set to update from [here](https://files.noodlebox.moe/userscripts/). You will most likely need a userscript manager, such as [Tampermonkey](https://tampermonkey.net/), which should be available for whichever browser you use. For userstyles, I recommend [Stylish](https://userstyles.org/).
 
 ## Scripts
 
@@ -8,6 +8,9 @@ Fix bug in gallery when too few items exist to allow scrolling. Bug was reported
 
 ### DiscordAutoGif.user.js (discordapp.com)
 Automatically play GIF and GIFV embeds without hovering. Note that animated content can eat a bit of CPU time (which is why auto-play is disabled in the first place).
+
+### DiscordLineNumbers.user.js (discordapp.com)
+Add line numbers to code blocks. It's recommended to add DiscordLineNumbers.css as a userstyle, if not using another theme that includes style rules for this.
 
 ### DiscordOwnerTag.user.js (discordapp.com)
 Show a tag next to a Discord server owner's name. Simple feature addition, since there's nothing else in the UI to identify a server owner.
