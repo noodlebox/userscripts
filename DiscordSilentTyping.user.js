3c6ac2fab50ca0023c28e5a31d6fef77c6f562e0
noodlebox <ash@noodlebox.moe>
2018-06-16 11:10:14 -0500

    Bump DiscordSilentTyping version to 2.1.2

--- DiscordSilentTyping.user.js
+++ DiscordSilentTyping.user.js
@@ -2,7 +2,7 @@
 // @name         DiscordSilentTyping
 // @namespace    https://files.noodlebox.moe/
 // @downloadURL  https://files.noodlebox.moe/userscripts/DiscordSilentTyping.user.js
-// @version      2.1.1
+// @version      2.1.2
 // @description  Don't send typing notifications
 // @author       noodlebox
 // @match        *://*.discordapp.com/channels/*
